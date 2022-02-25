78d7b58e775429349e60f5d04a9877e6d7bca823
문 성 운 <mkik90911@naver.com>
2022-02-25 16:28:44 +0900

    Lambda Ex

--- app/src/main/java/com/moon/kotlinstudy3/MainActivity.kt
+++ app/src/main/java/com/moon/kotlinstudy3/MainActivity.kt
@@ -2,10 +2,19 @@ package com.moon.kotlinstudy3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button)
+        val lambda = {x:Int, y:Int ->
+            Toast.makeText(this, "입력된 두 수는 ${x} 와 $y 입니다",Toast.LENGTH_SHORT).show()
+        }
+        btn.setOnClickListener {
+            lambda(20,10)
+        }
     }
 }
